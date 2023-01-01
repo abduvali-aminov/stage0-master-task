dc42b76f651e545b352f793c70608bd271955580
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2023-01-01 14:49:58 +0500

    ok

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,31 @@
 package com.epam.OOP;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String s = "This animal is mostly " + this.color +
+                ". It has "+ this.numberOfPaws;
+        if(this.numberOfPaws > 1){
+            s = s + " paws and ";
+        }else{
+            s = s + " paw and ";
+        }
+
+        if(this.hasFur){
+            s = s + "a fur.";
+        }else{
+            s = s + "no fur.";
+        }
+        return s;
+    }
 
 }
---
+++ src/main/java/com/epam/OOP/Main.java
@@ -0,0 +1,11 @@
+package com.epam.OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println(dog.getDescription());
+
+        Bird bird = new Bird();
+        System.out.println(bird.getDescription());
+    }
+}
