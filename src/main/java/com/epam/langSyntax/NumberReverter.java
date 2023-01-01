2e270563906c0a3275e41d173faa7e85d454f624
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2023-01-01 16:49:31 +0500

    ok

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,6 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        System.out.println((number % 10) * 100 + ((number / 10) % 10) * 10 + (number / 100));
     }
-
 }
